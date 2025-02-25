8311e847b7a6db2402bcafc1eed2e24b0ffb843e
MarcosDavi-Dev <adamsdavi77@gmail.com>
2025-02-25 20:00:41 -0300

    Estrutura do componente principal/Structure of main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+// Components
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>React MultiSetp</h1>
-    </>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para
+          avalair o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>Estapas</p>
+        <form>
+          <div className="inputs-container"></div>
+          <div className="actions">
+            <button type="button">
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button type="submit">
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
